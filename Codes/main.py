e45f72b3b80b334ad610ad919ef6e9b2bd6a76ac
CharlieYu4994 <charlieyu4994@outlook.com>
2020-08-01 20:19:20 +0800

    修复脑抽问题

--- Codes/main.py
+++ Codes/main.py
@@ -3,15 +3,34 @@ import utils
 
 main = Flask(__name__)
 
-@app.route('/encrypt/text', methods=['POST'])
+
+def load_key(_request):
+    prikey_t = _request['prikey']
+    thirdkey_t = _request['thirdkey']
+    passphrase = _request['passphrase']
+
+    return utils.load_key(thirdkey_t, prikey_t, passphrase)
+
+@main.route('/encrypt/text', methods=['POST'])
 def encrypt_text():
-    prikey_t = request.form['prikey']
-    thirdkey_t = request.form['thirdkey']
-    passphrase = request.form['passphrase']
+    status, prikey, thirdkey = load_key(request.form)
     message = request.form['message']
     sign_check = request.form ['sign_check']
 
-    status, prikey, thirdkey = utils.load_key(thirdkey_t, prikey_t, passphrase)
-    if not status: return False, '密钥错误'
+    if not status: return {'status': False, 'msg': '密钥错误'}
     enc_message = utils.encrypt_text(prikey, thirdkey, message, sign_check)
-    return True, enc_message
+    return {'status': True, 'msg': enc_message}
+
+@main.route('/decrypt/text', methods=['POST'])
+def decrypt():
+    status, prikey, thirdkey = load_key(request.form)
+    enc_message = request.form['message']
+
+    if not status: return False, '密钥错误'
+
+    _, status, message = utils.decrypt_text(prikey, thirdkey, enc_message)
+
+    if   status == -2: return {'status': False, 'sig': False, 'msg': '密文损坏'}
+    elif status == -1: return {'status': False, 'sig': False, 'msg': '密文无效'}
+    elif status ==  0: return {'status': True, 'sig': True, 'msg': message}
+    elif status ==  1: return {'status': True, 'sig': False, 'msg': message}
